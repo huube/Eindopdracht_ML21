2fc89122cba9f7eb44e8c475c8065eaa690c5a21
huube <huubelijzen@gmail.com>
2022-02-08 22:29:36 +0100

    dataprocessing

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,7 +1,15 @@
 from pathlib import Path
 from loguru import logger
+from loguru import logger
 import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.python.keras import layers
+from sklearn.preprocessing import OrdinalEncoder,OneHotEncoder, StandardScaler
+import sys
 
+sys.path.insert(0,'..')
 root = Path('..')
 
 
@@ -38,3 +46,79 @@ def create_labeled_data():
     df_out.to_csv(result_file,index=False)
 
     return df_out
+
+
+def create_train_test_validation():
+    """This function will create a train, test and validation set """
+
+    data_dir : Path = root / 'src' / 'data' / 'processed'
+    input_file : str = 'labeled_data.csv'
+    file_path : Path = data_dir / input_file
+
+    if file_path.exists():
+        logger.info(f"found file {input_file}, procceed with creating train, test and validation sets")
+        try: 
+            df = pd.read_csv(file_path)
+        except:
+            logger.info(f"an error occured while trying to pd.read_csv {file_path}")
+
+    else:
+        logger.info('Labeled data not found, creating a new file.')
+        create_labeled_data()
+    
+    ## remove these columns
+    columns_to_drop = [
+        'type',
+        'uri',
+        'analysis_url',
+        'time_signature',
+        'track_id',
+        'response_artist',
+        'response_track',
+        'spotify_id',
+    ]
+
+    df.drop(columns=columns_to_drop)
+
+    ## make these columns categorical variables
+
+    for col in ['key','mode','genre']:
+        df[col] = df[col].astype('category')
+
+
+    ##initialize encoders
+
+    ohe = OneHotEncoder(sparse=False)
+    scaler = StandardScaler()
+    oe = OrdinalEncoder()
+
+    ## Transform genre to ordinal coding
+    ordinaL_columns = oe.fit_transform(df['genre'])
+
+    ## apply OneHot encoding to categorical variables
+    columns_to_onehot = ['key']
+    onehot_columns = ohe.fit_transform(df[columns_to_onehot])
+
+    ## Apply StandardScaler to numeric values
+    columns_to_scale = [
+        'danceabilitiy',
+        'energy',
+        'loudness',
+        'speechiness',
+        'acousticness',
+        'instrumentalness',
+        'liveness',
+        'valence',
+        'tempo',
+        'duration_ms'
+    ]
+    
+    scaled_columns = scaler.fit_transform(columns_to_scale)
+
+    ## Not forget about the already binary feature 'mode'
+
+    binary_columns = df['mode'].to_numpy()
+
+    dataset = np.concatenate([onehot_columns,scaled_columns,binary_columns,ordinaL_columns],axis=1)
+
+
