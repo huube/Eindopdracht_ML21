a501e1d1bac610135df20f0b02cb22aab30064b4
huube <huubelijzen@gmail.com>
2022-02-12 15:28:01 +0100

    pre evaluation

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -16,14 +16,62 @@ from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras import Model
 from loguru import logger
 from pathlib import Path
-from ray.tune.integration.keras import TuneReportCallback
-from ray.tune.schedulers import AsyncHyperBandScheduler
-from ray.tune import JupyterNotebookReporter
-from ray import tune
-from src.data import make_dataset
-import ray
+
+
+import sys
+import keras_tuner as kt
+from kerastuner.tuners import RandomSearch, Hyperband
+from kerastuner.engine.hyperparameters import HyperParameters as hp
+from tensorflow.keras.models import Sequential
+
+sys.path.append("..")
+
+from definitions import get_project_root
+from src.data.make_dataset import create_train_test_validation
 
 from typing import Tuple, Optional, Union, Dict
 
-class BaseModel(tf.keras.Model):
-    
+x_train, x_valid, x_test, y_train, y_valid, y_test = create_train_test_validation()
+x_train.shape, y_train.shape, x_valid.shape, y_valid.shape, x_test.shape, y_test.shape
+
+
+def model_builder(hp):
+    model = Sequential()
+
+    model.add(Dense(23, activation="relu", input_shape=(23,)))
+
+    for i in range(hp.Int("n_layers", 1, 10)):
+        model.add(Dense(hp.Int(f"layer_{i}", 1, 200), activation="relu"))
+
+    model.add(Dropout(hp.Float("dropout", 0.05, 0.4)))
+    model.add(Dense(15, activation="softmax"))
+
+    model.compile(
+        optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+    )
+
+    return model
+
+
+def tune_random_search():
+    log_dir = "logs_kt_random"
+    tensorboard = TensorBoard(log_dir=log_dir)
+
+    tuner = RandomSearch(
+        model_builder,
+        objective="val_loss",
+        max_trials=15,
+        executions_per_trial=3,
+        directory=log_dir,
+    )
+
+    tuner.search_space_summary()
+
+    tuner.search(
+        x=x_train,
+        y=y_train,
+        epochs=10,
+        batch_size=64,
+        callbacks=[tensorboard],
+        validation_data=(x_valid, y_valid),
+    )
